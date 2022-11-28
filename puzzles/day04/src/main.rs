271b645d07c898aaf4b704bd117a57545602595a
olda <konency@inqool.cz>
2022-11-28 23:42:29 +0100

    initial commit

---
+++ puzzles/day04/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // let input = include_str!("../input.txt");
+    println!("Lets do this!");
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_example() {
+        assert_eq!(2, 1 + 1);
+    }
+
+}
