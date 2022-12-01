a59e357315d9565437c4d5b0f217e54938af5432
oldrich <oldik.kon@gmail.com>
2022-12-01 21:25:07 +0100

    Day1 correct naive implementation

--- puzzles/day01/src/main.rs
+++ puzzles/day01/src/main.rs
@@ -1,9 +1,23 @@
+use std::collections::BinaryHeap;
+
 fn main() {
-    // let input = include_str!("../input.txt");
-    println!("Lets do this!");
+    let mut heap = include_str!("../input.txt")
+        .split("\n\n")
+        .map(parse_and_sum_numbers)
+        .collect::<BinaryHeap<u64>>();
+
+    println!("First part: {}", heap.peek().unwrap());
+
+    let top3 = heap.pop().unwrap() + heap.pop().unwrap() + heap.pop().unwrap();
+
+    println!("Second part: {}", top3);
 }
 
-
+fn parse_and_sum_numbers(lines: &str) -> u64 {
+    lines.lines()
+        .map(|l| l.parse::<u64>().unwrap())
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
