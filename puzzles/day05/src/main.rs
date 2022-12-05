3f13915eb881da186b6ad4b5f1f171d697c49201
oldrich <oldik.kon@gmail.com>
2022-12-05 22:36:36 +0100

    Day5 correct naive impl

--- puzzles/day05/src/main.rs
+++ puzzles/day05/src/main.rs
@@ -1,3 +1,4 @@
+use once_cell::sync::Lazy;
 use regex::Regex;
 
 #[cfg(not(target_os = "windows"))]
@@ -7,14 +8,50 @@ const EMPTY_LINE_PATTERN: &str = "\r\n\r\n";
 
 const SPACE_BYTE_VALUE: u8 = 32;
 
+static REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"^move (\d+) from (\d+) to (\d+)$").unwrap());
+
 fn main() {
-    let (crates, instruction) = include_str!("../input.txt")
+    let (crates, ins) = include_str!("../input.txt")
         .split_once(EMPTY_LINE_PATTERN)
         .unwrap();
     let mut crates = parse_crates(crates);
-    let instructions = parse_instruction(instruction);
+    let instructions = parse_instruction(ins);
 
-    println!("{:?}", instructions);
+    println!(
+        "First part: {}",
+        first_solution(&instructions, crates.clone())
+    );
+    println!("Second part: {}", second_solution(&instructions, crates));
+}
+
+fn second_solution(instructions: &[Instruction], mut crates_plan: Vec<Vec<char>>) -> String {
+    for instruction in instructions {
+        let col = crates_plan.get_mut(instruction.from).unwrap();
+        let mut drained = col
+            .drain((col.len() - instruction.move_count)..)
+            .collect::<Vec<_>>();
+        crates_plan
+            .get_mut(instruction.to)
+            .unwrap()
+            .append(&mut drained);
+    }
+
+    crates_plan.iter().flat_map(|col| col.last()).collect()
+}
+
+fn first_solution(instructions: &[Instruction], mut crates_plan: Vec<Vec<char>>) -> String {
+    for instruction in instructions {
+        for _ in 0..instruction.move_count {
+            let c = crates_plan
+                .get_mut(instruction.from)
+                .unwrap()
+                .pop()
+                .unwrap();
+            crates_plan.get_mut(instruction.to).unwrap().push(c);
+        }
+    }
+
+    crates_plan.iter().flat_map(|col| col.last()).collect()
 }
 
 fn parse_crates(crates: &str) -> Vec<Vec<char>> {
@@ -50,11 +87,9 @@ fn parse_instruction(instructions: &str) -> Vec<Instruction> {
         .collect()
 }
 
-static REGEX: Regex = Regex::new(r"^move (\d+) from (\d+) to (\d+)$").unwrap();
-
 #[derive(Debug)]
 struct Instruction {
-    move_count: u32,
+    move_count: usize,
     from: usize,
     to: usize,
 }
