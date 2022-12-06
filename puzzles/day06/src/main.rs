f9c3ff284c34cec19e7fe91db2a980b0aa9592d2
oldrich <oldik.kon@gmail.com>
2022-12-06 21:12:51 +0100

    Day6 correct naive impl

--- puzzles/day06/src/main.rs
+++ puzzles/day06/src/main.rs
@@ -1,16 +1,32 @@
 fn main() {
-    // let input = include_str!("../input.txt");
-    println!("Lets do this!");
+    let input = include_str!("../input.txt");
+
+    println!(
+        "First: part: {}",
+        different_bytes_in_input(input, 4).unwrap()
+    );
+    println!(
+        "Second: part: {}",
+        different_bytes_in_input(input, 14).unwrap()
+    );
 }
 
+fn different_bytes_in_input(input: &str, window_size: usize) -> Option<usize> {
+    input
+        .as_bytes()
+        .windows(window_size)
+        .enumerate()
+        .find(|(index, window)| are_elements_different(window))
+        .map(|(index, window)| index + window.len())
+}
 
-
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn test_example() {
-        assert_eq!(2, 1 + 1);
+fn are_elements_different(slice: &[u8]) -> bool {
+    for i in 0..slice.len() - 1 {
+        for j in i + 1..slice.len() {
+            if slice[i] == slice[j] {
+                return false;
+            }
+        }
     }
-
+    true
 }
