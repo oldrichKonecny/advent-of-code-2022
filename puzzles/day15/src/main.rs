b82e5776cc14bf0909ba3b2ee8b14faa0c853424
olda <konency@inqool.cz>
2022-12-23 13:01:33 +0100

    day15 first part

--- puzzles/day15/src/main.rs
+++ puzzles/day15/src/main.rs
@@ -2,21 +2,45 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 
 static REGEX: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r"^Sensor at x=(\d+), y=(\d+): closest beacon is at x=(\d+), y=(\d+)$").unwrap()
+    Regex::new(r"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$")
+        .unwrap()
 });
 
 fn main() {
-    let input = include_str!("../input.txt")
+    let mut intervals = include_str!("../input.txt")
         .lines()
         .map(Sensor::parse)
+        .flat_map(|sensor| sensor.interval_on_y(2_000_000)) //10
         .collect::<Vec<_>>();
+    intervals.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut unique_intervals = Vec::new();
+    let mut possible_merge = 0;
+    unique_intervals.push(intervals.remove(0));
+    for i in intervals {
+        let int = unique_intervals.get_mut(possible_merge).unwrap();
+        if i.0 <= int.1 && i.1 > int.1 {
+            int.1 = i.1;
+        } else if i.0 > int.1 {
+            unique_intervals.push(i);
+            possible_merge += 1;
+        }
+    }
+
+    println!("{:?}", unique_intervals);
+
+    let res = unique_intervals
+        .iter()
+        .fold(0, |acc, val| acc + ((val.1 - val.0) + 1));
+
+    println!("First res: {}", res);
 }
 
 #[derive(Debug)]
 struct Sensor {
     coordinates: (i32, i32),
     beacon_coordinates: (i32, i32),
-    radius: u32,
+    radius: i32,
 }
 
 impl Sensor {
@@ -29,12 +53,65 @@ impl Sensor {
         Self {
             coordinates: (sensor_x, sensor_y),
             beacon_coordinates: (beacon_x, beacon_y),
-            radius: (sensor_x - beacon_x).abs() as u32 + (sensor_y - beacon_y).abs() as u32,
+            radius: (sensor_x - beacon_x).abs() + (sensor_y - beacon_y).abs(),
         }
     }
 
     fn interval_on_y(&self, y: i32) -> Option<(i32, i32)> {
-        // let r = self.coordinates.1 -
-        todo!()
+        let r = (self.coordinates.1 - y).abs();
+        if r > self.radius {
+            None
+        } else {
+            let pl = self.radius - r;
+            let mut left = self.coordinates.0 - pl;
+            let mut right = self.coordinates.0 + pl;
+            if self.beacon_coordinates == (left, y) {
+                left += 1;
+            } else if self.beacon_coordinates == (right, y) {
+                right -= 1;
+            }
+            Some((left, right))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Sensor;
+
+    #[test]
+    fn sensor_interval_on_y_0_0_test() {
+        let sensor = Sensor::parse("Sensor at x=0, y=0: closest beacon is at x=0, y=10");
+        assert_eq!(sensor.radius, 10);
+
+        assert_eq!(sensor.interval_on_y(0), Some((-10, 10)));
+        assert_eq!(sensor.interval_on_y(10), Some((0, 0)));
+        assert_eq!(sensor.interval_on_y(-10), Some((0, 0)));
+        assert_eq!(sensor.interval_on_y(1), Some((-9, 9)));
+        assert_eq!(sensor.interval_on_y(-1), Some((-9, 9)));
+        assert_eq!(sensor.interval_on_y(5), Some((-5, 5)));
+        assert_eq!(sensor.interval_on_y(5), Some((-5, 5)));
+        assert_eq!(sensor.interval_on_y(11), None);
+        assert_eq!(sensor.interval_on_y(-11), None);
+        assert_eq!(sensor.interval_on_y(100), None);
+        assert_eq!(sensor.interval_on_y(-500), None);
+    }
+
+    #[test]
+    fn sensor_interval_on_y_8_7_test() {
+        let sensor = Sensor::parse("Sensor at x=8, y=7: closest beacon is at x=2, y=10");
+        assert_eq!(sensor.radius, 9);
+
+        assert_eq!(sensor.interval_on_y(7), Some((-1, 17)));
+        assert_eq!(sensor.interval_on_y(-2), Some((8, 8)));
+        assert_eq!(sensor.interval_on_y(16), Some((8, 8)));
+        assert_eq!(sensor.interval_on_y(-1), Some((7, 9)));
+        assert_eq!(sensor.interval_on_y(15), Some((7, 9)));
+        assert_eq!(sensor.interval_on_y(6), Some((0, 16)));
+        assert_eq!(sensor.interval_on_y(10), Some((2, 14)));
+        assert_eq!(sensor.interval_on_y(-3), None);
+        assert_eq!(sensor.interval_on_y(17), None);
+        assert_eq!(sensor.interval_on_y(100), None);
+        assert_eq!(sensor.interval_on_y(-500), None);
     }
 }
